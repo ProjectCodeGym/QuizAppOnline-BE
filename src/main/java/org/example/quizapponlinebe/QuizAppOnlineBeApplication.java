b75ee6cc4293de651552a1872bd7ddd3ba1d338e
nguyenxuandat252 <nguyenxuandat252@gmail.com>
2025-06-08 22:30:59 +0700

    first commit

---
+++ src/main/java/org/example/quizapponlinebe/QuizAppOnlineBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.quizapponlinebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppOnlineBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAppOnlineBeApplication.class, args);
+    }
+
+}
